51e081ae2e8f58c363ef1ded6014cc272998222b
Daniel Di Lorenzo <ddilorenzo@gmail.com>
2023-07-24 14:39:24 -0300

    error cambiaba miPerfil

--- src/redux/miPerfil/reducer.js
+++ src/redux/miPerfil/reducer.js
@@ -10,17 +10,21 @@ export const reducer = (state = initialState, action) => {
 		...state,
 	};
 
-	if (action.item) {
-		newState.timeStamp = new Date().getTime();
-		newState.ok = true;
-		Object.entries(action.item).forEach(([key, value]) => {
-			newState[key] = value;
-			if (key == "sectores" && value.length > 0) {
-				newState.sector = value[0];
+	switch (action.type) {
+		case SET:
+			if (action.item) {
+				newState.timeStamp = new Date().getTime();
+				newState.ok = true;
+				Object.entries(action.item).forEach(([key, value]) => {
+					newState[key] = value;
+					if (key == "sectores" && value.length > 0) {
+						newState.sector = value[0];
+					}
+				});
+			} else {
+				newState.ok = false;
 			}
-		});
-	} else {
-		newState.ok = false;
+			break;
 	}
 
 	return newState;
