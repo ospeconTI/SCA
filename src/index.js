dfe5a4a6d45ceea89f52e32b24de20bb2ac0b92b
Bruno Monfrinotti <brunomon@hotmail.com>
2024-05-17 11:16:01 -0300

    notificaciones

--- src/index.js
+++ src/index.js
@@ -14,12 +14,90 @@ import { viewManager } from "./views/manager";
 import { register as registerSW, activate as activateSW } from "./libs/serviceWorker";
 import { getAll as getProtocolos } from "./redux/protocolos/actions";
 
-if (process.env.NODE_ENV === "production") {
-    registerSW();
-    activateSW();
-}
+//if (process.env.NODE_ENV === "production") {
+registerSW();
+activateSW();
+//}
 
 store.dispatch(captureMedia());
 store.dispatch(getProtocolos());
+
+const poll = (since) => {
+    return fetch("http://localhost:7777/brunomon/json?poll=1&since=" + since)
+        .then((response) => response.text())
+        .then((texto) => texto.split("\n"));
+};
+async function pollAndSave() {
+    const requestDB = window.indexedDB.open("ntfy", 1);
+
+    requestDB.onerror = (event) => {
+        console.error("Database error: " + event.target.errorCode);
+    };
+
+    requestDB.onsuccess = async (event) => {
+        getProximos(event.target.result, addMensajes);
+    };
+
+    requestDB.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        const objectStore = db.createObjectStore("mensajes", { keyPath: "time" });
+        objectStore.transaction.oncomplete = (event) => {
+            //addMensajes(db);
+        };
+    };
+
+    const getProximos = (db, oncompleted) => {
+        const transaction = db.transaction("mensajes", "readwrite");
+        const mensajesObjectStore = transaction.objectStore("mensajes");
+        const cursorObjectStore = mensajesObjectStore.openCursor(null, "prev");
+
+        cursorObjectStore.onsuccess = async (e) => {
+            const cursor = e.target.result;
+            if (cursor) {
+                const nextTime = cursor.value.time + 1;
+                poll(nextTime).then((proximos) => oncompleted(proximos, db));
+                return;
+            }
+        };
+    };
+
+    const addMensajes = (mensajes, db) => {
+        const mensajesObjectStore = db.transaction("mensajes", "readwrite").objectStore("mensajes");
+        mensajes.forEach((mensaje) => {
+            const mensajeObject = JsonOrDefault(mensaje);
+            if (mensajeObject != null) {
+                const objectStoreRequest = mensajesObjectStore.add(mensajeObject);
+                objectStoreRequest.onsuccess = (event) => {
+                    showNotification(mensajeObject);
+                    //console.log(event);
+                };
+                objectStoreRequest.onerror = (err) => {
+                    //console.log(err);
+                };
+            }
+        });
+    };
+}
+const JsonOrDefault = (text) => {
+    try {
+        return JSON.parse(text);
+    } catch (error) {
+        return null;
+    }
+};
+const showNotification = (datos) => {
+    navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification("Notificacion SCA", {
+            body: datos.message,
+            icon: datos.icon,
+        });
+    });
+};
+
+Notification.requestPermission().then(async (result) => {
+    if (result === "granted") {
+        setInterval(await pollAndSave, 5000);
+    }
+});
 
 console.log("Sirviendo datos de :" + SERVICE_URL);
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
 const webpack = require("webpack");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
@@ -19,9 +20,16 @@ module.exports = merge(common, {
         publicPath: "/",
     },
     plugins: [
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: false,
+            maximumFileSizeToCacheInBytes: 5000000,
+        }),
         new webpack.DefinePlugin({
-            SERVICE_URL: JSON.stringify("http://localhost:4000"),
-            //SERVICE_URL: JSON.stringify("https://scatest.uocra.net"),
+            //SERVICE_URL: JSON.stringify("http://localhost:4000"),
+            SERVICE_URL: JSON.stringify("https://scatest.uocra.net"),
             AUTHENTICATION_URL: JSON.stringify("https://fronttest.uocra.net"),
             AYUDA_URL: JSON.stringify("https://amparostest.uocra.net/AmparosImagenes/getImagen/%7CAyuda%7CAyudaSCA.pdf"),
         }),
